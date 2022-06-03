5be48a155a190a5b5a332e4f42c192465eed2de9
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-04 01:01:56 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:beverr/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Beverr());
+    await tester.pumpWidget(Beverr());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
