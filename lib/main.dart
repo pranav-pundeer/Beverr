be80213e0e533381ca6ad32f66d989eab60dbf81
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-04 01:00:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
+import 'package:beverr/models/userModel.dart';
 import 'package:beverr/screens/wrapper.dart';
+import 'package:beverr/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const Beverr());
+  runApp(Beverr());
 }
 
 class Beverr extends StatelessWidget {
-  const Beverr({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Wrapper(),
+    return StreamProvider<UserModel?>.value(
+      value: AuthService().user,
+      initialData: null,
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Wrapper(),
+      ),
     );
   }
 }
