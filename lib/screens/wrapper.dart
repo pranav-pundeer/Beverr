877ecd5f8e95457113e3f675a75a19c269e65812
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-04 01:01:43 +0530

    Update wrapper.dart

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,18 @@
+import 'package:beverr/models/userModel.dart';
 import 'package:beverr/screens/authentication/authenticate.dart';
 import 'package:beverr/screens/home/homescreen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+
+    final user= Provider.of<UserModel?>(context);
+    print(user);
+
+    return user == null? Authenticate(): HomeScreen();
   }
 }
