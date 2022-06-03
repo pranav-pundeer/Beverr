68e9c94ba7545e1aceb5f13046b49b6b9a7c0c4a
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-04 01:01:46 +0530

    Update auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,21 +1,43 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import '../models/userModel.dart';
 
 class AuthService{
 
   final FirebaseAuth _auth= FirebaseAuth.instance;
+
+  //create custom UserModel from FirebaseUser/User
+  UserModel? _userFromFirebaseUser(User? result){
+    return result!= null? UserModel(uid: result.uid): null;
+  }
+
+  //create stream to listen to authentication changes
+  Stream<UserModel?> get user{
+    return _auth.authStateChanges()
+        .map(_userFromFirebaseUser);
+        //.map() maps the user it gets from firebase to our custom user UserModel
+  }
 
   //sign in anonymously
   Future signInAnonymously() async{
     try{
       UserCredential result=  await _auth.signInAnonymously();
       User? user= result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     }catch(e){
       if (kDebugMode) {
         print(e);
       }
       return null;
+    }
+  }
+
+  //sign out of the homescreen
+  Future signOut() async{
+    try{
+      return await _auth.signOut();
+    }catch(e){
+      print(e.toString());
     }
   }
 }
