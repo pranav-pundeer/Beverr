4b4a819b74091346defcd57ded9775ba2834935d
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-14 00:57:46 +0530

    Update register.dart

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-
 import '../../services/auth.dart';
 
 class Register extends StatefulWidget {
+
+  final Function toggleView;
+  const Register({required this.toggleView});
 
   @override
   State<Register> createState() => _RegisterState();
@@ -10,26 +12,41 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
 
+  //form key to enable form validation, because we dont want to send
+  //empty requests to firebase auth
+  final _formkey = GlobalKey<FormState>();
+
   final AuthService _auth= AuthService();
   String email= "";
   String password= "";
-
+  String errorMessage= "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
+        actions: [
+          FlatButton.icon(onPressed: (){
+            widget.toggleView();
+          },
+            icon: const Icon(Icons.person),
+            label: const Text('Sign In'),),
+        ],
         elevation: 0.0,
         backgroundColor: Colors.brown[400],
-        title: const Center(child: Text('Register To Beverr' )),
+        title: const Text('Register To Beverr' ),
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
         child: Form(
+          key: _formkey,
           child: Column(
             children: [
               TextFormField(
+                validator: (value){
+                  return value!.isEmpty? 'email is required': null;
+                },
                 decoration: const InputDecoration(
                   hintText: "Enter email",
                 ),
@@ -40,6 +57,9 @@ class _RegisterState extends State<Register> {
                 },
               ),
               TextFormField(
+                validator: (value){
+                  return value!.length< 6? 'password should be of atleast 6 characters': null;
+                },
                 obscureText: true,
                 decoration: const InputDecoration(
                   hintText: "Enter password",
@@ -53,13 +73,21 @@ class _RegisterState extends State<Register> {
               const SizedBox(height: 30,),
               RaisedButton(
                 onPressed: ()async{
+                  if(_formkey.currentState!.validate()){
+                    dynamic result= await _auth.registerWithMailAndPassword(email, password);
+                    if(result== null){
+                      print('no');
+                      setState(()=> errorMessage= 'please enter a valid email');
+                    }
+                  }
                 },
                 color: Colors.pink,
                 child: const Text('Sign Up', style: TextStyle(color: Colors.white),),
               ),
+              const SizedBox(height: 15,),
+              Text(errorMessage, style: const TextStyle(color: Colors.red,fontSize: 15),),
             ],
           ),
-
         ),
       ),
     );
