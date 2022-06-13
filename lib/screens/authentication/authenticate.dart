e17c10ed671c583a03833c618446b940e91db3d7
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-14 00:57:44 +0530

    Update authenticate.dart

--- lib/screens/authentication/authenticate.dart
+++ lib/screens/authentication/authenticate.dart
@@ -4,15 +4,23 @@ import 'package:flutter/material.dart';
 
 
 class Authenticate extends StatefulWidget {
-  const Authenticate({Key? key}) : super(key: key);
 
   @override
   State<Authenticate> createState() => _AuthenticateState();
 }
 
 class _AuthenticateState extends State<Authenticate> {
+
+  bool showSignIn= true;
+
+  void toggleView(){
+    setState((){
+      showSignIn= !showSignIn;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Register();
+    return (showSignIn? SignIn(toggleView: toggleView,): Register(toggleView: toggleView));
   }
 }
