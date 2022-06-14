048e0d908ca9df48f1eefddd3e337bc821ee3812
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-14 23:04:48 +0530

    Create text.dart

---
+++ lib/constants/text.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+InputDecoration textFieldDecoration= const InputDecoration(
+  fillColor: Colors.white,
+  filled: true,
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.white, width: 2),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.brown, width: 3),
+  ),
+);
