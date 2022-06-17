2c8647ea659bc21e2a0b191f99174b37e3f99ff4
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-18 00:00:43 +0530

    Update homescreen.dart

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -1,26 +1,36 @@
+import 'package:beverr/screens/home/usersList.dart';
 import 'package:beverr/services/auth.dart';
+import 'package:beverr/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
 
-    AuthService _auth= AuthService();
+    AuthService auth= AuthService();
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Beverr'),
-        actions: [
-          FlatButton.icon(icon: Icon(Icons.person), label: Text('sign out'),
-              onPressed:() async{
-                  await _auth.signOut();
-              }),
-        ],
+    return StreamProvider<QuerySnapshot?>.value(
+      value: DataBase().beverrUsersData,
+      initialData: null,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          title: const Text('Beverr'),
+          actions: [
+            FlatButton.icon(icon: const Icon(Icons.person), label: const Text('sign out'),
+                onPressed:() async{
+                    await auth.signOut();
+              },
+            ),
+          ],
+        ),
+        backgroundColor: Colors.brown[50],
+        body: UsersList(),
       ),
-      backgroundColor: Colors.brown[50],
     );
   }
 }
