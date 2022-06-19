1c606f0212e4ac12827abac2de84149b8649bab0
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-20 01:44:06 +0530

    Update usersList.dart

--- lib/screens/home/usersList.dart
+++ lib/screens/home/usersList.dart
@@ -1,8 +1,10 @@
+import 'package:beverr/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
 import '../../constants/UserTile.dart';
+import '../../models/userModel.dart';
 
 class UsersList extends StatefulWidget {
   const UsersList({Key? key}) : super(key: key);
@@ -14,21 +16,25 @@ class _UsersListState extends State<UsersList> {
   @override
   Widget build(BuildContext context) {
 
+    final currentUser= Provider.of<DocumentSnapshot?>(context);
     final beverrUsers= Provider.of<QuerySnapshot?>(context);
-    int len= beverrUsers!.size;
-    print(len);
-    final userData= (beverrUsers!.docs[0]);
-    print(userData.get('name'));
-    print(userData.get('sugar'));
+    int len= beverrUsers?.size?? 0;
 
-    return ListView.builder(
-        itemCount: len,
-        itemBuilder: (context, index){
-          return UserTile(name: beverrUsers.docs[index].get('name'),
-                          sugar: beverrUsers.docs[index].get('sugar'),
-                          strength: beverrUsers.docs[index].get('strength'),
-          );
-        }
+    return Align(
+      alignment: Alignment.topCenter,
+      child: ListView.builder(
+          reverse: true,
+          itemCount: len,
+          shrinkWrap: true,
+          itemBuilder: (context, index){
+              return UserTile(name: beverrUsers?.docs[index].get('name'),
+                sugar: beverrUsers?.docs[index].get('sugar'),
+                strength: beverrUsers?.docs[index].get('strength'),
+              );
+          },
+          physics: const BouncingScrollPhysics(),
+
+      ),
     );
   }
 }
