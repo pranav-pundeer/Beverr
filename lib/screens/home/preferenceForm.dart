c321d5acb41746d67a0f435c997225783a36467a
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-20 01:44:04 +0530

    Create preferenceForm.dart

---
+++ lib/screens/home/preferenceForm.dart
@@ -0,0 +1,114 @@
+import 'package:beverr/constants/loading.dart';
+import 'package:beverr/constants/text.dart';
+import 'package:beverr/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../models/userModel.dart';
+
+class PreferenceForm extends StatefulWidget {
+  const PreferenceForm({Key? key}) : super(key: key);
+
+  @override
+  State<PreferenceForm> createState() => _PreferenceFormState();
+}
+
+class _PreferenceFormState extends State<PreferenceForm> {
+
+  final _formKey= GlobalKey<FormState>();
+  final List<String> sugars= ['0', '1', '2', '3', '4'];
+
+  String? currentName;
+  String? currentSugar;
+  int? currentStrength;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final user= Provider.of<UserModel?>(context);
+
+    return StreamBuilder<DocumentSnapshot>(
+      stream: DataBase(uid: user!.uid).userData,
+      builder: (context, snapshot) {
+
+            if(snapshot.hasData){
+              DocumentSnapshot<Object?>? userData= snapshot.data;
+
+              return Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(child: const Text('Update Preferences', style: TextStyle(fontSize: 20),),),
+                    const SizedBox(height: 20,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: TextFormField(
+                        initialValue: userData?.get('name'),
+                        decoration: textFieldDecoration.copyWith(hintText: 'enter name'),
+                        validator: (val){
+                          val!.isEmpty? 'please enter a name': null;
+                        },
+                        onChanged: (val)=> setState((){
+                          currentName= val;
+                        }),
+                      ),
+                    ),
+                    const SizedBox(height: 30,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: DropdownButtonFormField(
+                        decoration: textFieldDecoration.copyWith(hintText: 'enter the number of sugars'),
+                        value: currentSugar?? userData?.get('sugar'),
+                        items: sugars.map((sugar) {
+                          return DropdownMenuItem(
+                            value: sugar,
+                            child: Text('$sugar sugars'),
+                          );
+                        }).toList(),
+                        onChanged: (sugar)=> setState((){
+                          currentSugar= sugar as String?;
+                        }),
+                      ),
+                    ),
+                    const SizedBox(height: 15,),
+                    Slider(
+                      min: 100,
+                      max: 900,
+                      divisions: 8,
+                      activeColor: Colors.brown[currentStrength??userData?.get('strength')],
+                      inactiveColor: Colors.brown[100],
+                      value: (currentStrength?? userData?.get('strength')).toDouble(),
+                      onChanged: (val)=> setState(()=> currentStrength= val.round()),
+                    ),
+
+                    const SizedBox(height: 5,),
+                    //slider
+                    const Center(child: Text('set coffee strength',)),
+                    const SizedBox(height: 65,),
+                    Center(
+                      child: RaisedButton(
+                          shape: const StadiumBorder(),
+                          color: Colors.brown,
+                          child: const Text('Update', style: TextStyle(color: Colors.white),),
+                          onPressed: () async{
+                            if(_formKey.currentState!.validate()){
+                              await DataBase(uid: user.uid).updateUserData(
+                              currentSugar?? userData!.get('sugar'),
+                              currentName?? userData!.get('name'),
+                              currentStrength ?? userData!.get('strength'));
+                              Navigator.pop(context);
+                            }
+                          }),
+                    ),
+                  ],
+                ),
+              );
+            }else{
+                return const Loading();
+            }
+      }
+    );
+  }
+}
+
