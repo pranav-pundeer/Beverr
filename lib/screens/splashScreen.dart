63a2e2a713a0ccc3a7d59001e93350eb9051b108
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-21 01:47:44 +0530

    Create splashScreen.dart

---
+++ lib/screens/splashScreen.dart
@@ -0,0 +1,161 @@
+import 'package:beverr/screens/wrapper.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import '../constants/splashConstants.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _coffeeController;
+  bool copAnimated = false;
+  bool animateCafeText = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _coffeeController = AnimationController(vsync: this);
+    _coffeeController.addListener(() {
+      if (_coffeeController.value > 0.7) {
+        _coffeeController.stop();
+        copAnimated = true;
+        setState(() {});
+        Future.delayed(const Duration(seconds: 1), () {
+          animateCafeText = true;
+          setState(() {});
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _coffeeController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: cafeBrown,
+      body: Stack(
+        children: [
+          // White Container top half
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 1000),
+            height: copAnimated ? screenHeight / 1.9 : screenHeight,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(copAnimated ? 40.0 : 0.0)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Visibility(
+                  visible: !copAnimated,
+                  child: Lottie.asset(
+                    'splashAssets/coffeesplash.json',
+                    controller: _coffeeController,
+                    onLoaded: (composition) {
+                      _coffeeController
+                        ..duration = composition.duration
+                        ..forward();
+                    },
+                  ),
+                ),
+                Visibility(
+                  visible: copAnimated,
+                  child: Image.asset(
+                    'splashAssets/coffeepic2.png',
+                    height: 190.0,
+                    width: 190.0,
+                  ),
+                ),
+                Center(
+                  child: AnimatedOpacity(
+                    opacity: animateCafeText ? 1 : 0,
+                    duration: const Duration(milliseconds: 1000 ),
+                    child: const Text(
+                      'B E V E R R',
+                      style: TextStyle(fontSize: 50.0, color: cafeBrown),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Text bottom part
+          Visibility(visible: copAnimated, child: const _BottomPart()),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Find The Best Coffee for You',
+              style: TextStyle(
+                  fontSize: 27.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            const SizedBox(height: 30.0),
+            Text('',
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.white.withOpacity(0.8),
+                height: 1.5,
+              ),
+            ),
+            const SizedBox(height: 50.0),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 150.0,
+                width: 150.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 2.0),
+                ),
+                child: FlatButton.icon(
+                    shape: CircleBorder(),
+                    onPressed: (){
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> const Wrapper()));
+                    },
+                    icon: const Icon(
+                      Icons.coffee,
+                      size: 40.0,
+                      color: Colors.white,
+                    ),
+                  label: const Text(''),
+                ),
+              ),
+            ),
+            const SizedBox(height: 50.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
