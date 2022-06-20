ce68ab0214fd8c6b3617ccab0cbeac0c57eeb690
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-21 01:47:38 +0530

    Update signIn.dart

--- lib/screens/authentication/signIn.dart
+++ lib/screens/authentication/signIn.dart
@@ -25,70 +25,103 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return loading? Loading(): Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        actions: [
-          FlatButton.icon(onPressed: (){
-            widget.toggleView();
-          },
-            icon: const Icon(Icons.person),
-            label: const Text('Register'),),
-        ],
-        elevation: 0.0,
-        backgroundColor: Colors.brown[400],
-        title: const Text('Hi! Let\'s get you signed in' ),
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        child: Form(
-          key: _formkey,
-          child: Column(
-            children: [
-              TextFormField(
-                validator: (value){
-                  return (value!.isEmpty? 'please enter a valid email': null);
-                },
-                decoration: textFieldDecoration.copyWith(hintText: 'Enter email'),
-                onChanged: (value){
-                  setState((){
-                    email= value;
-                  });
-                },
-              ),
-              const SizedBox(height: 20,),
-              TextFormField(
-                validator: (value){
-                  return (value!.length<6? 'please enter a valid password (atleast 6 characters long)': null);
-                },
-                obscureText: true,
-                decoration: textFieldDecoration.copyWith(hintText: 'Enter password'),
-                onChanged: (value){
-                  setState((){
-                    password= value;
-                  });
-                },
-              ),
-              const SizedBox(height: 30,),
-              RaisedButton(
-                onPressed: ()async{
-                  if(_formkey.currentState!.validate()){
-                    setState(()=>loading= true);
-                    dynamic result= await _auth.signInWithEmailAndPassword(email, password);
-                    if(result== null){
-                      setState((){
-                        error= 'couldn\'t sign in with those credentials';
-                        loading= false;
-                      });
-                    }
-                  }
-                },
-                color: Colors.pink,
-                child: const Text('Sign In', style: TextStyle(color: Colors.white),),
-              ),
-              const SizedBox(height: 15,),
-              Text(error,style: TextStyle(color: Colors.pink),),
-            ],
+    return loading? Loading(): SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.brown[100],
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/coffee.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(height: 50,),
+                const Center(child: Text('Hi! Sign in to',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white70,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60))),
+                const Center(child: Text('Beverr',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 60))),
+                const SizedBox(height: 20,),
+                Center(
+                  child: Row(
+                    children: [
+                      SizedBox(width: 90,),
+                      const Center(child: Text('New to Beverr? ',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white70,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20))),
+                      FlatButton(
+                          onPressed: (){
+                          widget.toggleView();
+                          },
+                          child: Text('Register', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),))
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                TextFormField(
+                  validator: (value){
+                    return (value!.isEmpty? 'please enter a valid email': null);
+                  },
+                  decoration: textFieldDecoration.copyWith(hintText: 'Enter email', enabledBorder: InputBorder.none, focusedBorder: InputBorder.none, ),
+                  onChanged: (value){
+                    setState((){
+                      email= value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 20,),
+                TextFormField(
+                  validator: (value){
+                    return (value!.length<6? 'please enter a valid password (atleast 6 characters long)': null);
+                  },
+                  obscureText: true,
+                  decoration: textFieldDecoration.copyWith(hintText: 'Enter password'),
+                  onChanged: (value){
+                    setState((){
+                      password= value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 30,),
+                Container(
+                  height: 60,
+                  width: 300,
+                  child: RaisedButton(
+                    visualDensity: VisualDensity.adaptivePlatformDensity,
+                    shape: StadiumBorder(),
+                    onPressed: ()async{
+                      if(_formkey.currentState!.validate()){
+                        setState(()=>loading= true);
+                        dynamic result= await _auth.signInWithEmailAndPassword(email, password);
+                        if(result== null){
+                          setState((){
+                            error= 'couldn\'t sign in with those credentials';
+                            loading= false;
+                          });
+                        }
+                      }
+                    },
+                    color: Colors.brown[500],
+                    child: const Text('Sign In', style: TextStyle(color: Colors.white, ),),
+                  ),
+                ),
+                const SizedBox(height: 15,),
+                Text(error,style: TextStyle(color: Colors.pink),),
+              ],
+            ),
           ),
         ),
       ),
