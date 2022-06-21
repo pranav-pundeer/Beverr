397748405a50885e7064983dd52a60a86f2cbfbd
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-22 00:54:15 +0530

    Update database.dart

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,19 +1,19 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:beverr/models/userModel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DataBase{
 
-  final String? uid;
-  DataBase({this.uid} );
+  final String uid;
+  DataBase({required this.uid} );
 
   final CollectionReference BeverrUsers= FirebaseFirestore.instance.collection('Users');
 
-  Future updateUserData(String sugar, String name, int strength)async{
+  Future updateUserData(String sugar, String name, int strength, String email)async{
     return await BeverrUsers.doc(uid).set({
       'sugar': sugar,
       'name' : name,
       'strength': strength,
+      'email': email,
     });
   }
 
@@ -22,8 +22,19 @@ class DataBase{
     return BeverrUsers.snapshots();
   }
 
+  //userDataModel from firestore document snapshot
+  UserDataModel _userDataModelFromSnapshot(DocumentSnapshot snapshot){
+    return UserDataModel(
+      uid: uid,
+      name: snapshot.get('name'),
+      sugar: snapshot.get('sugar'),
+      strength: snapshot.get('strength'),
+      email: snapshot.get('email'),
+    );
+  }
+
   //get the stream of document snapshot of a particular user at the current time
-  Stream<DocumentSnapshot> get userData{
-      return BeverrUsers.doc(uid).snapshots();
+  Stream<UserDataModel> get userData{
+      return BeverrUsers.doc(uid).snapshots().map((_userDataModelFromSnapshot));
   }
 }
