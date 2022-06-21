015e6713b1b1e766877535f0b4e6a16f6474e5a1
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-22 00:53:58 +0530

    Update userModel.dart

--- lib/models/userModel.dart
+++ lib/models/userModel.dart
@@ -2,3 +2,13 @@ class UserModel{
   final String uid;
   UserModel({required this.uid});
 }
+
+class UserDataModel {
+  final String uid;
+  final String name;
+  final String sugar;
+  final int strength;
+  final String email;
+
+  UserDataModel({required this.uid, required this.name, required this.sugar, required this.strength, required this.email});
+}
