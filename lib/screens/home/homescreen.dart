4c4628dece3e68237d77cddada919186a1c0db1f
pranav-pundeer <pranavpundeer2304@gmail.com>
2022-06-22 00:54:03 +0530

    Update homescreen.dart

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -3,32 +3,24 @@ import 'package:beverr/screens/home/usersList.dart';
 import 'package:beverr/services/auth.dart';
 import 'package:beverr/services/database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../../models/userModel.dart';
+import 'currentUserWidget.dart';
 
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    final beverrUsers= Provider.of<QuerySnapshot?>(context);
+    final user= Provider.of<UserModel?>(context);
     AuthService auth= AuthService();
 
-    void showBottomSheetPanel(){
-      showModalBottomSheet(context: context, builder: (context){
-        return Flexible(
-          child: Container(
-            height: (MediaQuery.of(context).size.height) / 1.75,
-            child: const Padding(
-              padding: EdgeInsets.only(top:25.0),
-              child: PreferenceForm(),
-            ),
-          ),
-        );
-      });
-    }
 
     return StreamProvider<QuerySnapshot?>.value(
-      value: DataBase().beverrUsersData,
+      value: DataBase(uid: user!.uid).beverrUsersData,
       initialData: null,
       child: Scaffold(
         appBar: AppBar(
@@ -36,16 +28,11 @@ class HomeScreen extends StatelessWidget {
           elevation: 0.0,
           title: const Text('Beverr'),
           actions: [
-            FlatButton.icon(icon: const Icon(Icons.person), label: const Text('sign out'),
+            FlatButton.icon(icon: const Icon(Icons.logout), label: const Text('sign out'),
                 onPressed:() async{
                     await auth.signOut();
               },
             ),
-            FlatButton.icon(icon: const Icon(Icons.edit), label: const Text('edit'),
-              onPressed: () {
-                showBottomSheetPanel();
-              }
-              ),
           ],
         ),
         backgroundColor: Colors.brown[50],
@@ -56,7 +43,14 @@ class HomeScreen extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            child: const UsersList(),
+            child: ListView(
+              children:[
+                CurrentUserWidget(),
+                Divider(thickness: 2),
+                Center(child: Text('Mind Others\' Choices')),
+                UsersList(),
+              ],
+            ),
         ),
       ),
     );
